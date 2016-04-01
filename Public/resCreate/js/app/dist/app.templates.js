a4d9e971822730206b571425b4c80ddd8001ce49
马工 <89369750@qq.com>
2016-04-01 15:51:18 +0800

    架构与demo说明

---
+++ Public/resCreate/js/app/dist/app.templates.js
@@ -0,0 +1,10 @@
+angular.module('cityMap').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('Public/resCreate/html/src/public/areaKey/keyShow.cityMap.keyShowSelect.directive.html',
+    "<div id=\"keyShow\">\n" +
+    "    测试 directive\n" +
+    "</div>"
+  );
+
+}]);
---
+++ Public/resCreate/js/app/dist/appDev.js
@@ -0,0 +1,29 @@
+/** 开发js  */
+
+/** 库  */
+document.write('<script src="/Public/resCreate/js/lib/bower_components/angular/angular.js"><\/script>');
+document.write('<script src="/Public/resCreate/js/lib/bower_components/angular-bindonce/bindonce.js"><\/script>');
+
+/** module  */
+document.write('<script src="/Public/resCreate/js/app/src/aModule/aDipan.g.config.module.js"><\/script>');
+
+/** controller  */
+document.write('<script src="/Public/resCreate/js/app/src/controller/cityMap.g.body.controller.js"><\/script>');
+
+/** directive  */
+document.write('<script src="/Public/resCreate/js/app/src/directive/angularEnd.dipan.angularEnd.directive.js"><\/script>');
+document.write('<script src="/Public/resCreate/js/app/src/directive/areaKey/areaKey.cityMap.topAreaKey.directive.js"><\/script>');
+
+
+/** filter  */
+document.write('<script src="/Public/resCreate/js/app/src/filter/toHtml.cityMap.toHtml.filter.js"><\/script>');
+
+/** server  */
+document.write('<script src="/Public/resCreate/js/app/src/server/cityMap.fun.reBindOnce.factory.js"><\/script>');
+document.write('<script src="/Public/resCreate/js/app/src/server/compile.uiBlock.compile.factory.js"><\/script>');
+document.write('<script src="/Public/resCreate/js/app/src/server/tools.cityMap.block.factory.js"><\/script>');
+
+/** tpl  */
+document.write('<script src="/Public/resCreate/js/app/dist/app.templates.js"><\/script>');
+
+
